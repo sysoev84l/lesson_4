3836825bd2641623e362a01d75970d11eb43ebbe
sysoev84l <sysoev84l@outlook.com>
2021-01-23 11:42:33 +0300

    lesson_4

--- git_push.sh
+++ git_push.sh
@@ -2,7 +2,7 @@ CURRENT=`pwd`
 BASENAME=`basename "$CURRENT"`
 
 #comment="lesson_4"
-notify-send "git commit and push $BASENAME"
+notify-send "git commit and push $BASENAME" -i "/home/sysoev84l/Изображения/git/GitHub-Mark/PNG/GitHub-Mark-120px-plus.png"
 git add .
 git commit -m $BASENAME
 git push -u origin main
